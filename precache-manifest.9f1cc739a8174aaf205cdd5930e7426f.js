c1c562dead984409c03ac11825df7ffdbf3c41e2
iamohsin <59395132+iamohsin@users.noreply.github.com>
2020-01-16 16:36:12 +0500

    Updates

---
+++ precache-manifest.9f1cc739a8174aaf205cdd5930e7426f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a72ec26b45cff8c3be24e7c1cd1944",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3eddb803ce6a03c8e942",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "92e40de196923e096cc9",
+    "url": "/robofriends/static/css/main.68111d46.chunk.css"
+  },
+  {
+    "revision": "3eddb803ce6a03c8e942",
+    "url": "/robofriends/static/js/2.d2f9d004.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.d2f9d004.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92e40de196923e096cc9",
+    "url": "/robofriends/static/js/main.425782ab.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  }
+]);
